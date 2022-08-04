85909d9f91797041773a311688420455624a1303
Anwesha-dash811 <anweshagcekjr@gmail.com>
2022-08-04 13:04:50 +0530

    new modifications done

--- src/main/groovy/com/myapp/spring/DevopsGroovyProductsApplication.groovy
+++ src/main/groovy/com/myapp/spring/DevopsGroovyProductsApplication.groovy
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
 @SpringBootApplication
 class DevopsGroovyProductsApplication extends SpringBootServletInitializer{
 
